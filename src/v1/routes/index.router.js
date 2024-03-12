6440a37930a527ecb43d7995de4d5c99313b39f0
VuongHyKhang <hykhang792002@gmail.com>
2024-03-12 07:58:10 +0700

    ok

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -3,12 +3,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/shopping', require('./shopping'));
 router.use('/auth', require('./authentication'));
-router.use('/product', require('./product'));
-router.use('/register', require('./register'));
-router.use('/login', require('./login'));
-router.use('/user', require('./user'));
-
 
 module.exports = router;
